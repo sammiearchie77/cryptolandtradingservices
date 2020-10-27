d885834ae40458445686cc939a5ef4eb2d1ae9f7
Joseph <braidej@gmail.com>
2020-10-27 09:51:58 +0100

    major updates

--- cryptoland/cryptoland/settings.py
+++ cryptoland/cryptoland/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
